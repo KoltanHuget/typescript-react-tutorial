69ec0766d310f5c65e60cbd7ad7036f0fb8c32b3
Koltan Huget <koltanhuget@hotmail.com>
2022-04-01 09:46:59 -0600

    Tutorial complete

--- client/src/components/SingleTodo.tsx
+++ client/src/components/SingleTodo.tsx
@@ -1,52 +1,116 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {Todo} from '../model'
 import {AiFillEdit, AiFillDelete} from 'react-icons/ai'
 import {MdDone} from 'react-icons/md'
 import './styles.css'
+import { Draggable } from 'react-beautiful-dnd'
+
 
 interface Props {
+  index: number;
   todo: Todo;
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const SingleTodo = ({todo, todos, setTodos}: Props) => {
+const SingleTodo = ({todo, todos, setTodos, index, setCompletedTodos}: Props) => {
 
-  const handleDone = (id: number) => {
-    setTodos(todos.map((todo)=>{
-      if (todo.id === id) {
-        return {...todo, isDone: !todo.isDone}
-      }
-      return todo
-    }))
-  }
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleDone = async (current: Todo) => {
+    let isCompleted: boolean = true
+    await setTodos((old)=> {
+      return old.filter((todo)=> {
+        if (todo.id !== current.id) {
+          return todo
+        }
+        console.log('todos')
+        isCompleted = false
+      })
+    })
+    await setCompletedTodos((old)=> {
+      return old.filter((todo)=> {
+        if (todo.id !== current.id) {
+          return todo
+        }
+        console.log('completed')
+        isCompleted = true
+      })
+    })
+    if (isCompleted) {
+      setTodos((prev)=> {
+        return [...prev, current]
+      })
+    } else {
+      setCompletedTodos((prev)=> {
+        return [...prev, current]
+      })
+    }
+    
+      
+    }
+  
 
   const handleDelete = (id: number) => {
     setTodos(todos.filter((todo)=>{
       return todo.id !== id
     }))
+    setCompletedTodos((old) => old.filter((todo)=>{
+      return todo.id !== id
+    }))
   }
 
-  return (
-    <form className="todos_single">
-      {todo.isDone ? (
-        <s className='todos_single_text'>{todo.todo}</s>
-      ) : (
-        <span className='todos_single_text'>{todo.todo}</span>
-      )}
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(todos.map((todo)=>{
+      return todo.id===id? {...todo, todo: editTodo} : todo
+    }))
+    setEdit(false);
+  }
 
-      <div>
-        <span className="icon">
-          <AiFillEdit />
-        </span>
-        <span className="icon" onClick={()=>handleDelete(todo.id)}>
-          <AiFillDelete />
-        </span>
-        <span className="icon" onClick={()=>handleDone(todo.id)}>
-          <MdDone />
-        </span>
-      </div>
-    </form>
+  useEffect(()=> {
+    inputRef.current?.focus();
+  }, [edit])
+
+  return (
+    <Draggable draggableId={todo.id.toString()} index={index}>
+      {(provided, snapshot)=> (
+        <form {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} 
+        className={`todos_single ${snapshot.isDragging? "drag" : ""}`} 
+        onSubmit={(e)=>handleEdit(e, todo.id)}>
+
+        {edit ? (<input ref={inputRef} 
+        type='input' 
+        value={editTodo} 
+        onChange={(e)=>setEditTodo(e.target.value)} 
+        className='todos_single_text'/>) : (
+            <span className='todos_single_text'>{todo.todo}</span>
+          )
+        }
+
+        <div>
+          <span className="icon" onClick={()=> {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit)
+            }
+          }}>
+            <AiFillEdit />
+          </span>
+          <span className="icon" onClick={()=>handleDelete(todo.id)}>
+            <AiFillDelete />
+          </span>
+          <span className="icon" onClick={()=>handleDone(todo)}>
+            <MdDone />
+          </span>
+        </div>
+      </form>
+      )}
+    
+    </Draggable>
   )
 }
 
